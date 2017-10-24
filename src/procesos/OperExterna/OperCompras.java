5999da4106262bf290f6f6ed46c412ba506fff08
CharlesHurtado <>
2017-10-24 15:28:52 -0500

    24 de Octubre de 2017 Charles Hurtado Ajuste Size de canvas con ranuras

---
+++ src/procesos/OperExterna/OperCompras.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procesos.OperExterna;
+
+import procesos.Operacion;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class OperCompras extends Operacion {
+    
+}
